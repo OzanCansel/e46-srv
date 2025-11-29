bb7cdb4eacfcee4fa4d7b2e00c0f539372e4a298
Ozan Cansel <ozancansel@gmail.com>
2025-11-29 11:14:03 +0300

    Accepted connections are registered to epoll. Echo back mechanism implemented. Need stress test to find flaws. Epoll Async Echo Server in C - Day 1

--- src/e46srv.c
+++ src/e46srv.c
@@ -3,11 +3,13 @@
 #include <errno.h>
 #include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/epoll.h>
 #include <arpa/inet.h>
 
 #define E46SRV_LISTEN_BACKLOG 256
 #define MAX_EVENTS            256
+#define ECHO_BUF_SIZE         4096
 
 static int make_socket_nonblocking(int fd)
 {
@@ -64,7 +66,7 @@ int e46srv_listen(struct e46srv_cfg* cfg, struct e46srv_ctx *ctx)
     {
         fprintf(
             stderr,
-            "Could not bind. err: (%d)%s\n",
+            "Could not bind. err: %d(%s)\n",
             errno,
             strerror(errno)
         );
@@ -78,7 +80,7 @@ int e46srv_listen(struct e46srv_cfg* cfg, struct e46srv_ctx *ctx)
     {
         fprintf(
             stderr,
-            "Could not listen. err: (%d)%s\n",
+            "Could not listen. err: %d(%s)\n",
             errno,
             strerror(errno)
         );
@@ -88,11 +90,11 @@ int e46srv_listen(struct e46srv_cfg* cfg, struct e46srv_ctx *ctx)
 
     int nonblock_res = make_socket_nonblocking(srv_fd);
 
-    if (nonblock_res < 0)
+    if (nonblock_res == -1)
     {
         fprintf(
             stderr,
-            "Could not make server socket non-blocking. err: (%d)%s",
+            "Could not make server socket non-blocking. err: %d(%s)",
             errno,
             strerror(errno)
         );
@@ -107,7 +109,7 @@ int e46srv_listen(struct e46srv_cfg* cfg, struct e46srv_ctx *ctx)
     {
         fprintf(
             stderr,
-            "Could not create epoll structure. err: (%d)%s\n",
+            "Could not create epoll structure. err: %d(%s)\n",
             errno,
             strerror(errno)
         );
@@ -124,7 +126,7 @@ int e46srv_listen(struct e46srv_cfg* cfg, struct e46srv_ctx *ctx)
     {
         fprintf(
             stderr,
-            "Could not register listener for server sck. err: (%d)%s\n",
+            "Could not register listener for server sck. err: %d(%s)\n",
             errno,
             strerror(errno)
         );
@@ -156,7 +158,7 @@ int e46srv_listen(struct e46srv_cfg* cfg, struct e46srv_ctx *ctx)
                     // TODO: Handle error here
                     fprintf(
                         stderr,
-                        "Cannot accept client. err: (%d)%s\n",
+                        "Cannot accept client. err: %d(%s)\n",
                         errno,
                         strerror(errno)
                     );
@@ -167,8 +169,127 @@ int e46srv_listen(struct e46srv_cfg* cfg, struct e46srv_ctx *ctx)
 
                     ntop(&client_addr, CLI_IP_STR);
 
-                    // Client accepted
                     printf("Accepted new client. addr: %s\n", CLI_IP_STR);
+
+                    ep_req.events  = EPOLLIN;
+                    ep_req.data.fd = client_fd;
+
+                    if (make_socket_nonblocking(client_fd) == -1)
+                    {
+                        fprintf(
+                            stderr,
+                            "Could not make client socket non-blocking. fd: %d, err: %d(%s)\n",
+                            client_fd,
+                            errno,
+                            strerror(errno)
+                        );
+
+                        close(client_fd);
+
+                        continue;
+                    }
+
+                    // Inform kernel to listen `client_fd` as well.
+                    if (epoll_ctl(ctx->epoll_fd, EPOLL_CTL_ADD, client_fd, &ep_req) == -1)
+                    {
+                        fprintf(
+                            stderr,
+                            "Could not register read event for client. fd: %d, err: %d(%s)\n",
+                            client_fd,
+                            errno,
+                            strerror(errno)
+                        );
+
+                        close(client_fd);
+                    }
+                }
+            }
+            else
+            {
+                char READ_BUF[ECHO_BUF_SIZE];
+                int client_fd = e->data.fd;
+
+                // What we are doing is trying to implement an echo server.
+                // So to accomplish this we need to echo back what we have read.
+                // At the time of `recv` called, we dont know whether we will succeed
+                // to send data back. So that's why we dont drain buffer and use `MSG_PEEK`
+                int npeek = recv(client_fd, READ_BUF, sizeof(READ_BUF) - 1, MSG_PEEK);
+
+                // We have read data
+                if (npeek > 0)
+                {
+                    READ_BUF[npeek] = '\0';
+
+                    printf("fd: %d => %s", client_fd, READ_BUF);
+
+                    ssize_t nwritten = send(client_fd, READ_BUF, npeek, 0);
+
+                    // Successfull write
+                    if (nwritten > 0)
+                    {
+                        ssize_t ndrained = recv(client_fd, READ_BUF, nwritten, 0);
+
+                        if (ndrained < 0)
+                        {
+                            // We have a big problem here
+                            // at least, report it
+                            fprintf(
+                                stderr,
+                                "ERR! Echoed back %ld bytes successfully but could not drained. "
+                                "fd: %d, ret: %ld, err: %d(%s)\n",
+                                nwritten,
+                                client_fd,
+                                ndrained,
+                                errno,
+                                strerror(errno)
+                            );
+
+                            // We lost the integrity, at least close the fd;
+                            close(client_fd);
+                        }
+                        else if (ndrained == 0) // Is it possible to peer closed connection ?
+                        {
+                            // If this is the case, no problem
+                            fprintf(
+                                stderr,
+                                "Echoed back and client closed. Looks strange. fd: %d\n",
+                                client_fd
+                            );
+
+                            close(client_fd);
+                        }
+                        else if (ndrained != nwritten)
+                        { // Ok, we drained buffer. I hope ndrained == nwritten
+                            fprintf(
+                                stderr,
+                                "ERR! Could not drained same amount of bytes echoed back. "
+                                "fd: %d, nwritten: %ld, ndrained: %ld\n",
+                                client_fd,
+                                nwritten,
+                                ndrained
+                            );
+                        }
+                    }
+                }
+                else if (npeek == 0) // Means, client closed the connection.
+                {
+                    printf("Peer closed the connection. fd: %d\n", client_fd);
+
+                    close(client_fd);
+                }
+                else if (npeek != EAGAIN)
+                {
+                    // Handle error
+                    fprintf(
+                        stderr,
+                        "ERR! Closing socket. fd: %d, code: %d, err: %d(%s)\n",
+                        client_fd,
+                        npeek,
+                        errno,
+                        strerror(errno)
+                    );
+
+                    close(client_fd);
                 }
             }
         }
